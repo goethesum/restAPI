f65f9d96d1e950bbf5644e12aab72b1534296dc8
herman <goethesum@gmail.com>
2022-02-08 22:11:13 +0500

    implement comment package

--- internal/comment/comment.go
+++ internal/comment/comment.go
@@ -31,3 +31,58 @@ func NewService(db *gorm.DB) *Service {
 		DB: db,
 	}
 }
+
+// GetComment - retrieves comment by their ID from the database
+func (s *Service) GetComment(ID uint) (Comment, error) {
+	var comment Comment
+	if result := s.DB.First(&comment, ID); result.Error != nil {
+		return Comment{}, result.Error
+	}
+	return comment, nil
+}
+
+// GetCommentsBySlug retrieves all comments by slug (path - /article/name )
+func (s *Service) GetCommentBySlug(slug string) ([]Comment, error) {
+	var comments []Comment
+	if result := s.DB.Find(&comments).Where("slug = ?", slug); result.Error != nil {
+		return []Comment{}, result.Error
+	}
+	return comments, nil
+}
+
+// PostComment - adds a new comment to the database
+func (s *Service) PostComment(Comment Comment) (Comment, error) {
+	if result := s.DB.Save(&Comment); result.Error != nil {
+		return Comment, result.Error
+	}
+	return Comment, nil
+}
+
+// UpdateComment - updates a comment by ID with new comment line
+func (s *Service) UpdateComment(ID uint, newComment Comment) (Comment, error) {
+	comment, err := s.GetComment(ID)
+	if err != nil {
+		return Comment{}, err
+	}
+	if result := s.DB.Model(&comment).Updates(newComment); result.Error != nil {
+		return Comment{}, result.Error
+	}
+	return comment, nil
+}
+
+// DeleteComment - deletes a comment from the database by ID
+func (s *Service) DeleteComment(ID uint) error {
+	if result := s.DB.Delete(&Comment{}, ID); result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+// GetAllComments - retrieves all comments from the database
+func (s *Service) GetAllComments() ([]Comment, error) {
+	var comments []Comment
+	if result := s.DB.Find(&comments); result.Error != nil {
+		return comments, result.Error
+	}
+	return comments, nil
+}
